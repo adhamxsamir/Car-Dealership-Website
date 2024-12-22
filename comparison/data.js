78ccd88f0d7b16bffec73b281fc8930400a4ba07
Mohamed Essam elkerkary <alihesham2782008@gamil.com>
2024-12-22 11:37:25 +0200

    added the js file

---
+++ comparison/data.js
@@ -0,0 +1,84 @@
+var car = [{
+    "id" : 0 ,
+    "title" : "",
+    "description" : "",
+    "color" : "",
+    "price" : "",
+    "Image" : "search-car-sign-vector-magnifying-600nw-538584514.webp"
+},
+{
+    "id" : 1 ,
+    "title" : "Aston Martin Vantage ",
+    "description" : "this is the 2024 brand new ASTON MARTIN VANTAGE ,with 0 miles ,good condition makes you feel like wealthy ,aston martin conitnue with thier gasoline motor (4.0 Twin-Turbo V8 Engine. Aston Martin Vantage Horsepower: 503 horsepower (665PS) Maximum Torque:800 NM)",
+    "color" : "White",
+    "price" : "$200,000",
+    "Image" : "Aston Martin Vantage.avif"
+},
+{
+    "id" : 2 ,
+    "title" : "BMW M5",
+    "description" : "this is the brand new 2025 BMW M5 , with 0 miles ,this car had many comments when bmw relasied it but, BMW said its the strongest M5 ever with there new hybrid& gasoline motor s68 (petrol engine gives 585HP, 750 NM & the electric engine gives 197 HP ,250 NM overall 727 HP & 1000NM)",
+    "color" : "black Metallic",
+    "price" : "$123,000",
+    "Image" : "BMW M5.avif"
+},
+{
+    "id" : 3 ,
+    "title" : "BMW M3",
+    "description" : "Brand new BMW M3 used with 10k milage ,All M3 models are powered by a twin-turbo 3.0-liter inline-six sends 473 HP and 406 pound-feet of torque",
+    "color" : "White",
+    "price" : "$76,000",
+    "Image" : "BMW M3.jpg"
+},
+{
+    "id" : 4 ,
+    "title" : "Audi R8 V10 plus",
+    "description" : "Brand new 2024 Audi R8 V10 plus , with 0 miles (5.2-liter V10 engine 602 hp, 610 PS)",
+    "color" : "Black",
+    "price" : "$180,000",
+    "Image" : "Audi R8 V10 Plus.jpg"
+},
+{
+    "id" : 5 ,
+    "title" : "lamborghini huracan",
+    "description" : "Brand new 2024 lamborghini huracan , with 0 miles (5.2-liter V10 engine 602 hp, 610 PS)",
+    "color" : "Red",
+    "price" : "$200,000",
+    "Image" : "Lamborghini Huracan.jpg"
+},
+{
+    "id" : 6 ,
+    "title" : "Hummer EV SUV 3x",
+    "description" : "The Hummer EV currently offers either two or three electric motors. Both configurations offer all-wheel drive; the dual-motor setup has 570 hp and the tri-motor version has 1000 hp.",
+    "color" : "white",
+    "price" : "$100,000",
+    "Image" : "Hummer EV SUV 3X.avif"
+},
+{
+    "id" : 7 ,
+    "title" : "Genesis Electrified G80",
+    "description" : "he Electrified G80 is a force of nature. With 365 Horsepower * , 516 lb.-ft. of torque and a dual motor AWD system",
+    "color" : "Silver",
+    "price" : "$80,000",
+    "Image" : "Genesis Electrified G80.avif"
+},
+{
+    "id" : 8 ,
+    "title" : "chevrolet camaro",
+    "description" : "the Chevrolet Camaro ZL1 has a 6.2-liter supercharged LT4 V8 rated at 650 hp and 650 lb-ft of torque.",
+    "color" : "Black",
+    "price" : "$75,000",
+    "Image" : "chevrolet camaro.png"
+},
+{
+    "id" : 9 ,
+    "title" : "Ford Ranger",
+    "description" : "Ford Ranger comes with  270-horsepower turbocharged 2.3-liter four-cylinder and a 315-horsepower twin-turbo V-6 , with 12,000 miles ,powered by desiel",
+    "color" : "Silver",
+    "price" : "$40,000",
+    "Image" : "Ford Ranger.png"
+}
+
+
+]
+
